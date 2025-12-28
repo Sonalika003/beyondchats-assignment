dc4b7770530f339534937dc231ab6c0fd56f0cba
Sonalika Chaudhari <sonalikachaudhari43@gmail.com>
2025-12-29 01:26:21 +0530

    Added BeyondChats blog scraper utility

---
+++ backend/src/utils/scrapeBeyondChats.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import cheerio from "cheerio";
+
+const BLOG_URL = "https://beyondchats.com/blogs/";
+
+export const scrapeBeyondChatsBlogs = async () => {
+  const { data } = await axios.get(BLOG_URL);
+  const $ = cheerio.load(data);
+
+  const blogLinks = [];
+
+  $(".blog-card a").each((_, element) => {
+    const link = $(element).attr("href");
+    if (link) {
+      blogLinks.push(`https://beyondchats.com${link}`);
+    }
+  });
+
+  // Get last 5 (oldest)
+  const oldestBlogs = blogLinks.slice(-5);
+
+  return oldestBlogs;
+};
