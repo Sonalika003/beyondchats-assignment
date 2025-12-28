1a8b3c8895019ca43b3ccf7173aafbbbe845a846
Sonalika Chaudhari <sonalikachaudhari43@gmail.com>
2025-12-29 01:28:47 +0530

    Added article controller to create article from blog URL

---
+++ backend/src/controllers/articleController.js
@@ -0,0 +1,27 @@
+import Article from "../models/Article.js";
+import { scrapeSingleBlog } from "../utils/scrapeSingleBlog.js";
+
+export const createArticleFromUrl = async (req, res) => {
+  try {
+    const { url } = req.body;
+
+    if (!url) {
+      return res.status(400).json({ message: "URL is required" });
+    }
+
+    const scrapedData = await scrapeSingleBlog(url);
+
+    const article = await Article.create({
+      title: scrapedData.title,
+      originalContent: scrapedData.content,
+      sourceUrl: scrapedData.sourceUrl
+    });
+
+    res.status(201).json({
+      success: true,
+      data: article
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
