5285b8dd54b7c340fa979bf07fd8f4ebffb1d5a8
Sonalika Chaudhari <sonalikachaudhari43@gmail.com>
2025-12-29 01:34:48 +0530

    Registered article routes in server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./src/config/db.js";
+import articleRoutes from "./src/routes/articleRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("BeyondChats API running");
 });
+
+app.use("/api/articles", articleRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
